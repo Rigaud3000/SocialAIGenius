dc0a57bfb99b7c66d3ee88c5d2d3f11b72774877
GrandMasterZoe <123234117+Rigaud3000@users.noreply.github.com>
2025-04-05 20:12:30 -0400

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -57,11 +57,11 @@ app.use((req, res, next) => {
   }
 
   // ALWAYS serve the app on port 5000
-const port = 5000;
+const port = process.env.PORT || 10000;
 server.listen({
   port,
   host: "0.0.0.0",
   reusePort: true,
 }, () => {
   log(`serving on port ${port}`);
-})();
+});
