6d345e1503beb0e22caea646f0b30be46ddd4b31
GrandMasterZoe <123234117+Rigaud3000@users.noreply.github.com>
2025-04-09 18:16:07 -0400

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,8 @@ import express, { type Request, Response, NextFunction } from "express";
 import cors from "cors";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
